39e6fc8a5fa87db7afba2f5f8994c87e9b765e96
Jesus <jesus.fdz93@gmail.com>
2023-11-18 12:56:22 -0600

    async

---
+++ js/async_await.js
@@ -0,0 +1,35 @@
+// BACKEND
+const promesa = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    let correcto = true;
+
+    if (correcto) {
+      resolve("Promesa resuelta");
+    } else {
+      reject("Promesa rechazada");
+    }
+  }, 1000);
+});
+
+// FRONTEND con async - await
+const resolver = async () => {
+  try {
+    // const respuesta = await fetch("https://fakestoreapi.com/products");
+    // const datos = await respuesta.json();
+    const respuesta = await promesa;
+    console.log(respuesta);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+resolver();
+
+// FRONTEND con then - catch
+// promesa
+//   .then((resultado) => {
+//     console.log(resultado);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
