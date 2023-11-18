9ee92308a2d03370b2f0432bd2de215ca43ef2e2
Jesus <jesus.fdz93@gmail.com>
2023-11-18 12:14:06 -0600

    promesas

--- js/promesas.js
+++ js/promesas.js
@@ -22,3 +22,29 @@ resolver
   .finally(() => {
     console.log("Esto se ejecuta siempre");
   });
+
+// BACKEND
+const saludar = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    let frutas = ["manzana", "zanahoria", "pera"];
+
+    const mayusculas = frutas.map((fruta) => {
+      return fruta.toUpperCase();
+    });
+
+    if (mayusculas.length > 0) {
+      resolve(mayusculas);
+    } else {
+      reject("No se encontraron resultados");
+    }
+  }, 4000);
+});
+
+// FRONTEND
+saludar
+  .then((resultado) => {
+    console.log(resultado);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
